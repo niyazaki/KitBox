17e45213a37e581e11981d6347529987de40c77e
Niyazaki <zakkassabeh@hotmail.com>
2019-02-20 23:36:20 +0100

    Merged all classes in one file
    Cannot delete old files, so they're just empty right now.

---
+++ KitBoxProgram/KitBoxProgram/classes.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitBoxProgram
+{
+    class Locker
+    {
+    }
+
+    abstract class Rail
+    {
+    }
+    class railLR : Rail
+    {
+    }
+    class railFR : Rail
+    {
+    }
+    class railBA : Rail
+    {
+    }
+
+    abstract class Panel
+    {
+    }
+    class panelUD : Panel
+    {
+    }
+    class panelLR : Panel
+    {
+    }
+    class panelBA : Panel
+    {
+    }
+
+    class Cabinet
+    {
+    }
+    class Door
+    {
+    }
+    class Cleat
+    {
+    }
+    class Angle
+    {
+    }
+
+
+}
