8c12b0bcaa5861ae7d07a8d9f5bafa8f52bf955e
Niyazaki <zakkassabeh@hotmail.com>
2019-02-25 17:46:56 +0100

    Add DB

---
+++ KitBoxProgram/KitBoxProgram/SQLiteDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitBoxProgram
+{
+    public class SQLiteDataAccess
+    {
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
