a89e7bbc1535944d080376cf52353dbfe38df130
Hana-Jabbour <36258812+Hana-Jabbour@users.noreply.github.com>
2019-02-26 12:00:04 +0100

    DataBase
    Database: création database et liason de celle-ci avc projet.
    Programs: contient commandes pour ajouter et selectionner données de BD

---
+++ KitBoxProgram/KitBoxProgram/Database.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.SQLite;
+using System.IO;
+
+namespace SQLiteDemo
+{
+    class Database
+    {
+        public SQLiteConnection myConnection;
+
+        public Database()
+        {
+            myConnection = new SQLiteConnection("Data Source= catalogue.db");
+            if (!File.Exists("./catalogue.db"))
+            {
+                SQLiteConnection.CreateFile("catalogue.db");
+                System.Console.WriteLine("Database file created");
+            }
+
+
+        }
+        public void OpenConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+
+            }
+        }
+        public void CloseConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Closed)
+            {
+                myConnection.Close();
+            }
+
+        }
+
+      
+   
+    }
+}
---
+++ KitBoxProgram/KitBoxProgram/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace SQLiteDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database databaseObject = new Database();
+
+            /**
+             * INSERT INTO DB
+             **
+            
+            string qwery = "INSERT INTO table ('id1','id2') VALUES (@id1,@id2)"; //Par ex table album avec titre(id1), artiste(id2)
+            SQLiteCommand myCommand = new SQLiteCommand(query, databaseObject.myConnection);
+            databaseObject.myConnection.Open();
+            myCommand.Parameters.AddWhitValues("@id1", "MetsCeQueTuVeux"); //@titre, ex: Halo
+            myCommand.Parameters.AddWhitValues("@id2", "MetsCeQueTuVeux"); //@artiste, ex: Beyoncé
+            var result = myCommand.ExecuteNonQuery();
+            databaseObject.myConnection.Close();
+
+            Console.WriteLine("Rows Added: {0}", result); //Dis qu'un élément vient d'être ajouté à la DB. Ainsi t'ajoute  un par un les élem dans ta table
+            */
+
+
+
+            /**
+             * SELECT FROM DB
+             **
+            
+            string qwery = "SELECT * FROM table";
+            SQLiteCommand myCommand = new SQLiteCommand(query, databaseObject.myConnection);
+            databaseObject.myConnection.Open();
+            myCommand.Parameters.AddWhitValues("@id1", "MetsCeQueTuVeux"); //@titre, ex: Halo
+            myCommand.Parameters.AddWhitValues("@id2", "MetsCeQueTuVeux"); //@artiste, ex: Beyoncé
+            SQLiteDataReader result = myCommand.ExecuteReader();
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    Console.WriteLine("table : {0} - id2 : {1}", result["id2"], result["id1"]); //Si on run, on verra avec l'ex du dessus: Album : Halo - Artiste: Beyoncé et chaque nouvelle ligne correspond à une musique et son artiste
+                }
+            }
+            databaseObject.myConnection.Close();
+    */
+
+            Console.ReadKey();
+
+        }
+    }
+}
