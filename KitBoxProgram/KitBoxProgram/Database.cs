966860d502d3dda4b22016c60b4150e115e0f6a1
Niyazaki <zakkassabeh@hotmail.com>
2019-02-26 13:48:43 +0100

    Update Db.db

--- KitBoxProgram/KitBoxProgram/Database.cs
+++ KitBoxProgram/KitBoxProgram/Database.cs
@@ -9,21 +9,19 @@ namespace SQLiteDemo
 
         public Database()
         {
-            myConnection = new SQLiteConnection("Data Source= catalogue.db");
-            if (!File.Exists("./catalogue.db"))
-            {
-                SQLiteConnection.CreateFile("catalogue.db");
-                System.Console.WriteLine("Database file created");
-            }
-
-
+            myConnection = new SQLiteConnection("Data Source= Db.db");
+        //This part is useless because we don't want to create a new DB
+//            if (!File.Exists("./catalogue.db"))
+//            {
+//                SQLiteConnection.CreateFile("catalogue.db");
+//                System.Console.WriteLine("Database file created");
+//            }
         }
         public void OpenConnection()
         {
             if (myConnection.State != System.Data.ConnectionState.Open)
             {
                 myConnection.Open();
-
             }
         }
         public void CloseConnection()
@@ -32,10 +30,6 @@ namespace SQLiteDemo
             {
                 myConnection.Close();
             }
-
         }
-
-      
-   
     }
 }
