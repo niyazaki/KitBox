8e29bc0fb60394f98d04a0f609649ff34f65af53
El bakkoury Yassine <16050@ecam.be>
2019-02-28 15:29:55 +0100

    Add methods

--- KitBoxProgram/KitBoxProgram/classes.cs
+++ KitBoxProgram/KitBoxProgram/classes.cs
@@ -10,9 +10,18 @@ namespace KitBoxProgram
     {
         List<Box> boxes;
         double price;
+        public void AddBox(Box box)
+        {
+            boxes.Add(box);
+        }
 
         public double GetPrice()
         {
+            price = 0;
+            foreach (Box i in boxes)
+            {
+                price += i.price;
+            }
             return price;
         }
     }
@@ -22,7 +31,7 @@ namespace KitBoxProgram
         string color;
         int height;
         bool hasDoor;
-        double price;
+        public double price;
 
         List<Accessory> accessories;
 
@@ -40,16 +49,16 @@ namespace KitBoxProgram
 
         public void AddAccessory(Accessory accessory)
         {
-
-        }
-
-        public void SetPrice()
-        {
-
+            accessories.Add(accessory);
         }
 
         public double GetPrice()
         {
+            price = 0;
+            foreach(Accessory i in accessories)
+            {
+                price += i.price;
+            }
             return price;
         }
     }
