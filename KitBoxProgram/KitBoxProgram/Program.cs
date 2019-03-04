30eb67c8436751ce69f1ac4df516a98c9c7c8f59
Niyazaki <zakkassabeh@hotmail.com>
2019-03-04 22:16:26 +0100

    Added how to add values into db
    Erevything is in comment but should work perfectly (not tested).

--- KitBoxProgram/KitBoxProgram/Program.cs
+++ KitBoxProgram/KitBoxProgram/Program.cs
@@ -23,6 +23,18 @@ namespace SQLiteDemo
             Message.Show(e.ToString());
             Message.Show("Connexion failed !");
           }
+
+          //If we wanna add an item to the db (must be done in a method)
+          /*
+
+          MySqlCommand cmd = new MySqlCommand("INSERT into nomdelatable(colonne concernée 1, colonne concernée 2,...) VALUES(@parametre1 ex:nom colonne1, @parametre2)", connection)
+          cmd.Parameters.AddWithValue("@parametre1", valeur1);
+          cmd.Parameters.AddWithValue("@parametre2", valeur2);
+          cmd.ExecuteNonQuery();
+          cmd.Parameters.Clear();
+
+          */
+
         }
     }
 }
