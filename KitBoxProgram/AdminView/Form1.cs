dbc05600f26ecffca458c2843384b27f85fe19c2
El Haddadi Yassine <36259631+16146@users.noreply.github.com>
2019-03-25 22:59:15 +0100

    Update AdminView

--- KitBoxProgram/AdminView/Form1.cs
+++ KitBoxProgram/AdminView/Form1.cs
@@ -25,18 +25,46 @@ namespace AdminView
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string myConnectionString = "SERVER=db4free.net;" + "DATABASE=kitbox;" + "UID=kitbox;" + "PASSWORD=ecamgroupe4;"+ "OldGuids=True;";
-            MySqlConnection connection = new MySqlConnection(myConnectionString);
-
             try
             {
-               connection.Open(); //The exception is thrown HERE
+                string myConnection = "SERVER=db4free.net;" + "DATABASE=kitbox;" + "UID=kitbox;" + "PASSWORD=ecamgroupe4;" + "OldGuids=True;";
+                MySqlConnection myConn = new MySqlConnection(myConnection);
+                MySqlDataAdapter myDataAdapter = new MySqlDataAdapter();
+                myDataAdapter.SelectCommand = new MySqlCommand(" select * database.edata ;", myConn);
+                MySqlCommandBuilder cb = new MySqlCommandBuilder(myDataAdapter);
+                myConn.Open();
+                MessageBox.Show("Connected");
+                myConn.Close();
+                button1.Visible = false;
+                button2.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string myConnection = "SERVER=db4free.net;" + "DATABASE=kitbox;" + "UID=kitbox;" + "PASSWORD=ecamgroupe4;" + "OldGuids=True;";
+            MySqlConnection myConn = new MySqlConnection(myConnection);
+            MySqlCommand commandDB = new MySqlCommand(" select * from Command ;", myConn);
+            try
+            {
+                MySqlDataAdapter sda = new MySqlDataAdapter();
+                sda.SelectCommand = commandDB;
+                DataTable dbdataset = new DataTable();
+                sda.Fill(dbdataset);
+                BindingSource bSource = new BindingSource();
+
+                bSource.DataSource = dbdataset;
+                dataGridView1.DataSource = bSource;
+                sda.Update(dbdataset);
 
             }
-            catch (MySqlException ex)
+            catch(Exception ex)
             {
-                String error = ex.Message;
-                
+                MessageBox.Show(ex.Message);
             }
         }
     }
