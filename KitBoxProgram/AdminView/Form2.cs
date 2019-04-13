cbba84a85e61852f742e9ec30cd2af4fd7dc9ee3
El Haddadi Yassine <36259631+16146@users.noreply.github.com>
2019-04-13 22:30:29 +0200

    Revert ":("
    This reverts commit a4b9c0bbb11e83b854df541e68008835e28f6457.

---
+++ KitBoxProgram/AdminView/Form2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TestView
+{
+    public partial class Form2 : System.Windows.Forms.UserControl
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Enter your name please!");
+            }
+            else
+            {
+               
+            }
+        }
+
+        private void Admin_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            this.Controls.Add(new SellerView());
+        }
+    }
+}
+
